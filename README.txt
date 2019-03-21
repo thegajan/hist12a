d67ae95b6492a236455a62f7308e2a6eaa4937ce
Gajan Nagaraj <thegajannagaraj@gmail.com>
2019-03-21 10:39:47 -0700

    final

---
+++ README.txt
@@ -0,0 +1,1 @@
+To run this website, extract the contents of the zip folder and then click on "index.html." The website should pop. Enjoy!
---
+++ js/main.js
@@ -0,0 +1,15 @@
+﻿
+$(document).ready(function() {
+  $("#arrow").click(function() {
+    $("html, body").animate({
+      scrollTop: 0
+    }, "slow");
+    return false;
+  });
+  $("#bib").click(function() {
+    $("#bib-full").fadeIn('slow');
+  });
+  $("#close").click(function() {
+    $("#bib-full").fadeOut('slow');
+  });
+});
